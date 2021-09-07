4736a33555a3b9559062ccd0e86b770d58a96bcf
jt <1216414009@qq.com>
2021-09-07 21:15:59 +0800

    thread safe hashmap

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -16,6 +16,7 @@ struct entry {
 struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
+pthread_mutex_t locks[NBUCKET];
 
 double
 now()
@@ -39,6 +40,7 @@ static
 void put(int key, int value)
 {
   int i = key % NBUCKET;
+  pthread_mutex_lock(&locks[i]);
 
   // is the key already present?
   struct entry *e = 0;
@@ -53,6 +55,7 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
+  pthread_mutex_unlock(&locks[i]);
 }
 
 static struct entry*
@@ -115,6 +118,10 @@ main(int argc, char *argv[])
     keys[i] = random();
   }
 
+  // alloc the big lock 
+  for (int i = 0; i < NBUCKET; i++) {
+    pthread_mutex_init(&locks[i], NULL);
+  }
   //
   // first the puts
   //
