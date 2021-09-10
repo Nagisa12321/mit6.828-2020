254d7d1617e500f6978477c2354132aa44b6dff0
jt <1216414009@qq.com>
2021-09-10 14:55:02 +0800

    n cpus kalloc(Memory allocator )

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -21,12 +21,20 @@ struct run {
 struct {
   struct spinlock lock;
   struct run *freelist;
-} kmem;
+} kmem[NCPU];
 
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
+  int     i;
+
+  // init everylock, and free every range
+  for (i = 0; i < NCPU; i++) {
+    initlock(&kmem[i].lock, "kmem");
+  }
+  
+  // Let freerange give all free memory 
+  // to the CPU running freerange.
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -46,7 +54,8 @@ freerange(void *pa_start, void *pa_end)
 void
 kfree(void *pa)
 {
-  struct run *r;
+  struct run  *r;
+  int         cpu_id;
 
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
@@ -56,10 +65,17 @@ kfree(void *pa)
 
   r = (struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  // close the interrupt
+  push_off();
+  cpu_id = cpuid();
+
+  acquire(&kmem[cpu_id].lock);
+  r->next = kmem[cpu_id].freelist;
+  kmem[cpu_id].freelist = r;
+  release(&kmem[cpu_id].lock);
+
+  // open the interrupt
+  pop_off();
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -68,14 +84,39 @@ kfree(void *pa)
 void *
 kalloc(void)
 {
-  struct run *r;
+  struct run  *r;
+  int         cpu_id;
+  int         i;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
+  // close the interrupt
+  push_off();
+  cpu_id = cpuid();
+
+  acquire(&kmem[cpu_id].lock);
+  r = kmem[cpu_id].freelist;
   if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+    kmem[cpu_id].freelist = r->next;
+  else {
+    // steal the memory from other CPU
+    for (i = 0; i < NCPU; i++) {
+      if (i != cpu_id) {
+        acquire(&kmem[i].lock);
+        r = kmem[i].freelist;
+        if (r) {
+          // steal a page [r]
+          kmem[i].freelist = r->next;
+        }
+        release(&kmem[i].lock);
+      }
+      if (r)
+        break;
+    }
+  }
+  release(&kmem[cpu_id].lock);
 
+  // open the interrupt
+  pop_off();
+  
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
   return (void*)r;
